6b12ace256495a9cb992a23cd10294f27cc5715d
iamvk1437k <creativeunknowncoder@gmail.com>
2022-03-09 16:34:37 +0530

    theme install seperated and single file install declared

---
+++ linux/xubuntu/dracula-theme.sh
@@ -0,0 +1,20 @@
+###theming###
+
+#gtk theme 'dracula'
+cd /usr/share/themes ; pwd ;sudo axel https://github.com/dracula/gtk/releases/download/v3.0/Dracula.tar.xz ; sudo tar -xvf Dracula.tar.xz ; sudo rm -v *.tar.xz ; cd
+
+#icons 'dracula-icons' & #cursor theme 'dracula-cursors'
+cd /usr/share/icons ; pwd ; sudo axel https://github.com/dracula/gtk/releases/download/v3.0/Dracula-cursors.tar.xz ; sudo git clone https://github.com/m4thewz/dracula-icons ; sudo rm -vrf dracula-icons/.git ; sudo rm -v dracula-icons/Preview.png ; sudo tar -vxf Dracula-cursors.tar.xz ; rm -v *.tar.xz ; cd
+sudo gtk-update-icon-cache /usr/share/icons/dracula-icons/
+#xfce4-terminal "Dracula"
+cd ; git clone https://github.com/dracula/xfce4-terminal.git ; mkdir -pv ~/.local/share/xfce4/terminal/colorschemes ; cd xfce4-terminal ; cp -vrf Dracula.theme ~/.local/share/xfce4/terminal/colorschemes ; cd ; rm -vrf xfce4-terminal ; cd
+
+#rofi 'dracula'
+cd ; git clone https://github.com/dracula/rofi ; mkdir ~/.config/rofi ; cp -vrf rofi/theme/config1.rasi ~/.config/rofi/config.rasi ; cd ; rm -vrf rofi ; cd
+
+
+#mousepad 'dracula'
+cd ; git clone https://github.com/dracula/mousepad.git && cd mousepad ; mkdir -pv "$HOME/.local/share/gtksourceview-3.0/styles" ; cp dracula.xml $HOME/.local/share/gtksourceview-3.0/styles ; cd ; rm -vrf mousepad ; cd
+
+#wallpaper
+cd /usr/share/backgrounds ; sudo axel https://github.com/dracula/wallpaper/raw/master/xubuntu.png ; cd 
--- linux/xubuntu/xubuntuinitialsetup.sh
+++ linux/xubuntu/xubuntuinitialsetup.sh
@@ -10,55 +10,85 @@ sudo apt purge pidgin gnome-software xfce4-notes gnome-menus gnome-mines gnome-s
 
 ### install required tools ###
 
-sudo apt install font-manger rofi axel git mpv ffmpeg python3-pip gnome-disk-utility ncdu virtualbox virtualbox-ext-pack virtualbox-guest-additions-iso elinks w3m lynx
+sudo apt install font-manager rofi axel git mpv ffmpeg python3-pip gnome-disk-utility ncdu virtualbox virtualbox-ext-pack virtualbox-guest-additions-iso elinks w3m lynx git conky light
 
 #install fonts
-sudo apt install ifonts-dejavu fonts-firacode fonts-cantarell
+sudo apt install fonts-firacode fonts-cantarell
 
 #install yt-dlp , youtube-dl
-sudo apt purge youtube-dl ; pip3 install yt-dlp youtube-dl
+sudo apt purge youtube-dl ; pip3 install yt-dlp youtube-dl 
 sudo apt autoremove
 
-#install oh-my-zsh
-#https://ohmyz.sh/
-sudo apt install zsh curl ; sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 
 
-#librewolf
+#Install librewolf
 #https://deb.librewolf.net
-#Ubuntu 20.04 #https://deb.librewolf.net/pool/focal/main/libr/librewolf/
-#sudo gpkg -i *.deb
-
-
-#Telegram
-#https://desktop.telegram.org/
-#tar -xvf *.tar.xz ; mkdir -pv ~/.telegram ; cp -vrf Telegram/* ~/.telegram
-
-
-
-
-###theming###
-
-#gtk theme 'dracula'
-cd /usr/share/themes ; pwd ;sudo axel https://github.com/dracula/gtk/releases/download/v3.0/Dracula.tar.xz ; sudo tar -xvf Dracula.tar.xz ; sudo rm -v *.tar.xz ; cd
-
-#icons 'dracula-icons' & #cursor theme 'dracula-cursors'
-cd /usr/share/icons ; pwd ; sudo axel https://github.com/dracula/gtk/releases/download/v3.0/Dracula-cursors.tar.xz ; sudo git clone https://github.com/m4thewz/dracula-icons ; sudo rm -vrf dracula-icons/.git ; sudo rm -v dracula-icons/Preview.png ; sudo tar -vxf Dracula-cursors.tar.xz ; rm -v *.tar.xz ; cd
-
-#xfce4-terminal "Dracula"
-cd ; git clone https://github.com/dracula/xfce4-terminal.git ; mkdir -pv ~/.local/share/xfce4/terminal/colorschemes ; cd xfce4-terminal ; cp -vrf Dracula.theme ~/.local/share/xfce4/terminal/colorschemes ; cd ; rm -vrf xfce4-terminal ; cd
-
-#rofi 'dracula'
-cd ; git clone https://github.com/dracula/rofi ; mkdir ~/.config/rofi ; cp -vrf rofi/theme/config1.rasi ~/.config/rofi/config.rasi ; cd ; rm -vrf rofi ; cd
-
-
-#mousepad 'dracula'
-cd ; git clone https://github.com/dracula/mousepad.git && cd mousepad ; mkdir -pv "$HOME/.local/share/gtksourceview-3.0/styles" ; cp dracula.xml $HOME/.local/share/gtksourceview-3.0/styles ; cd ; rm -vrf mousepad ; cd
-
+sudo apt install wget
+echo "deb [arch=amd64] https://deb.librewolf.net $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/librewolf.list
+sudo wget https://deb.librewolf.net/keyring.gpg -O /etc/apt/trusted.gpg.d/librewolf.gpg 
+sudo apt update ; sudo apt install librewolf -y
 #librewolf extensions 
 echo "Installing Dracula theme for librewolf..."
 librewolf https://addons.mozilla.org/en-US/firefox/addon/dracula-dark-colorscheme/
 
 echo "Install dark viewer add-on in librewolf..."
 librewolf https://addons.mozilla.org/en-US/firefox/addon/darkreader/
+
+#install exa
+sudo apt install wget
+sudo apt install curl
+cd ; EXA_VERSION=$(curl -s "https://api.github.com/repos/ogham/exa/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+') 
+curl -Lo exa.zip "https://github.com/ogham/exa/releases/latest/download/exa-linux-x86_64-v${EXA_VERSION}.zip"
+sudo unzip -q exa.zip bin/exa -d /usr/local
+cd
+rm exa.zip
+
+#install advcpmv
+cd
+set -e
+ADVCPMV_VERSION=${1:-0.9}
+CORE_UTILS_VERSION=${2:-9.0}
+
+wget http://ftp.gnu.org/gnu/coreutils/coreutils-$CORE_UTILS_VERSION.tar.xz
+tar xvJf coreutils-$CORE_UTILS_VERSION.tar.xz
+rm coreutils-$CORE_UTILS_VERSION.tar.xz
+(
+    cd coreutils-$CORE_UTILS_VERSION/
+    wget https://raw.githubusercontent.com/jarun/advcpmv/master/advcpmv-$ADVCPMV_VERSION-$CORE_UTILS_VERSION.patch
+    patch -p1 -i advcpmv-$ADVCPMV_VERSION-$CORE_UTILS_VERSION.patch
+    ./configure
+    make
+    cp ./src/cp ../advcp
+    cp ./src/mv ../advmv
+)
+rm -rf coreutils-$CORE_UTILS_VERSION
+sudo cp -v advcp /usr/local/bin
+sudo cp -v advmv /usr/local/bin
+
+cd ; rm -vrf advcpmv
+
+#install xdm from https://github.com/subhra74/xdm/releases
+cd
+XDM_VER=$(curl -s "https://api.github.com/repos/subhra74/xdm/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+curl -Lo xdm.tar.xz "https://github.com/subhra74/xdm/releases/download/${XDM_VER}/xdm-setup-${XDM_VER}.tar.xz"
+tar -xvf xdm.tar.xz ; sudo bash install.sh ; rm -v install.sh readme.txt xdm.tar.xz ; cd  
+
+#install Telegram from https://github.com/telegramdesktop/tdesktop/releases/ 
+#https://desktop.telegram.org/
+cd
+TG_VER=$(curl -s "https://api.github.com/repos/telegramdesktop/tdesktop/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+curl -Lo tsetup.tar.xz "https://github.com/telegramdesktop/tdesktop/releases/download/v${TG_VER}/tsetup.${TG_VER}.tar.xz"
+tar -xvf tsetup.tar.xz ; mkdir -pv ~/.telegram ; cp -vrf Telegram/* ~/.telegram
+cd
+
+#install Bitwardern
+cd
+BITW_VER=$(curl -s "https://github.com/bitwarden/desktop/releases/latest" | grep releases | sed -E 's/.*"([^"]+)".*/\1/' | grep -Po "v\K[0-9.]+") 
+curl -Lo bitw.deb https://github.com/bitwarden/desktop/releases/download/v${BITW_VER}/Bitwarden-${BITW_VER}-amd64.deb
+sudo dpkg -i bitw.deb ; rm -v bitw.deb
+cd
+
+#install oh-my-zsh
+#HTTps://ohmyz.sh/
+sudo apt install zsh curl ; sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 
