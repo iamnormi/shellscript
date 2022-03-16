1686dec4c7c29fa7f99cf0a46951232c56efd1f2
iamvk1437k <creativeunknowncoder@gmail.com>
2022-03-17 02:47:31 +0530

    updated dracula theme

--- linux/xubuntu/dracula-theme.sh
+++ linux/xubuntu/dracula-theme.sh
@@ -4,7 +4,7 @@
 cd /usr/share/themes ; pwd ;sudo axel https://github.com/dracula/gtk/releases/download/v3.0/Dracula.tar.xz ; sudo tar -xvf Dracula.tar.xz ; sudo rm -v *.tar.xz ; cd
 
 #icons 'dracula-icons' & #cursor theme 'dracula-cursors'
-cd /usr/share/icons ; pwd ; sudo axel https://github.com/dracula/gtk/releases/download/v3.0/Dracula-cursors.tar.xz ; sudo git clone https://github.com/m4thewz/dracula-icons ; sudo rm -vrf dracula-icons/.git ; sudo rm -v dracula-icons/Preview.png ; sudo tar -vxf Dracula-cursors.tar.xz ; rm -v *.tar.xz ; cd
+cd /usr/share/icons ; pwd ; sudo axel https://github.com/dracula/gtk/releases/download/v3.0/Dracula-cursors.tar.xz ; sudo git clone https://github.com/m4thewz/dracula-icons ; sudo rm -vrf dracula-icons/.git ; sudo rm -v dracula-icons/Preview.png ; sudo tar -vxf Dracula-cursors.tar.xz ; sudo rm -v *.tar.xz ; cd
 sudo gtk-update-icon-cache /usr/share/icons/dracula-icons/
 #xfce4-terminal "Dracula"
 cd ; git clone https://github.com/dracula/xfce4-terminal.git ; mkdir -pv ~/.local/share/xfce4/terminal/colorschemes ; cd xfce4-terminal ; cp -vrf Dracula.theme ~/.local/share/xfce4/terminal/colorschemes ; cd ; rm -vrf xfce4-terminal ; cd
