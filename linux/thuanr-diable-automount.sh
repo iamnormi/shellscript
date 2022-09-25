d8f707a2da45b77ee6770892213bb3e924bf8ade
iamvk1437k <automatecode@gmail.com>
2022-09-25 11:17:16 +0530

    linux initial setup scripts removed

--- linux/thuanr-diable-automount.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#Disable drive automount in thunar file manager
-xfconf-query -c thunar-volman -p /automount-drives/enabled -s false # disable drive automount
-xfconf-query -c thunar-volman -p /automount-media/enabled -s false # disable media automount
-xfconf-query -c thunar-volman -p /autobrowse/enabled -s false # disable thunar popping up
